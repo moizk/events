c8b2deaf01724ea8fda87aeba3347c16670baa21
Alexander E. Dimitriyadi <alex@heuretix.com>
2013-03-29 20:37:47 -0300

    Fixed sort order.
    Timeline is going from earliest to latest, not vice versa. My bad.

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ActiveRecord::Base
   attr_accessible :event_end_date, :event_end_time, :event_start_date, :event_start_time, :location, :name
   belongs_to :organization
 
-  scope :today, lambda { where(event_start_date: Time.zone.now.all_day).order('events.event_start_time DESC') }
-  scope :tomorrow, lambda { where(event_start_date: 1.day.from_now.all_day).order('events.event_start_date, events.event_start_time DESC') }
-  scope :this_week, lambda { where(event_start_date: 3.day.from_now.all_week).order('events.event_start_date, events.event_start_time DESC') }
+  scope :today, lambda { where(event_start_date: Time.zone.now.all_day).order('events.event_start_time ASC') }
+  scope :tomorrow, lambda { where(event_start_date: 1.day.from_now.all_day).order('events.event_start_date, events.event_start_time ASC') }
+  scope :this_week, lambda { where(event_start_date: 3.day.from_now.all_week).order('events.event_start_date, events.event_start_time ASC') }
 end
