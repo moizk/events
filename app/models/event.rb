53ae139c91f8e90fa7c85c4ddb1e7f65c858f326
Moiz K <moiz@heuretix.com>
2013-03-29 16:14:14 -0400

    Model work for Today, Tommorrow, and This Week

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,10 @@
 class Event < ActiveRecord::Base
   attr_accessible :event_end_date, :event_end_time, :event_start_date, :event_start_time, :location, :name
   belongs_to :organization
+
+  scope :today, lambda { where(event_start_date: Time.zone.now.all_day) }
+  scope :tomorrow, lambda { where(event_start_date: 1.day.from_now.all_day) }
+  scope :this_week, lambda { where(event_start_date: Time.zone.now.all_week) }
+
+
 end
