794f27f93066ecff9cab3eaf0657fa2f60ad8929
Moiz K <moiz@heuretix.com>
2013-03-29 17:03:34 -0400

    Events for this week

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,5 @@ class Event < ActiveRecord::Base
 
   scope :today, lambda { where(event_start_date: Time.zone.now.all_day) }
   scope :tomorrow, lambda { where(event_start_date: 1.day.from_now.all_day) }
-  scope :this_week, lambda { where(event_start_date: Time.zone.now.all_week) }
-
-
+  scope :this_week, lambda { where(event_start_date: 3.day.from_now.all_week) }
 end
