f08087e23450f8afceb8576ec43dc0e019df0c59
Alexander E. Dimitriyadi <alex@heuretix.com>
2013-03-29 21:08:07 -0300

    Scopes are actually class methods.

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,7 +5,7 @@ class Event < ActiveRecord::Base
   scope :today, lambda { where(event_start_date: Time.zone.now.all_day).order('events.event_start_time ASC') }
   scope :tomorrow, lambda { where(event_start_date: 1.day.from_now.all_day).order('events.event_start_date, events.event_start_time ASC') }
   
-  def this_week
+  def self.this_week
     two_days = Time.now.beginning_of_day + 2.days
     one_week = Time.now.end_of_day + 7.days
     where('events.event_start_date >= ? AND events.event_start_date <= ?', two_days, one_week).order('events.event_start_date, events.event_start_time ASC')
