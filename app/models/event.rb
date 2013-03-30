315d3ffeb5741f46d756d5f873fdcb5d9aa1e047
Alexander E. Dimitriyadi <alex@heuretix.com>
2013-03-29 21:06:26 -0300

    Fixed rails parsing syntax

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,8 @@ class Event < ActiveRecord::Base
 
   scope :today, lambda { where(event_start_date: Time.zone.now.all_day).order('events.event_start_time ASC') }
   scope :tomorrow, lambda { where(event_start_date: 1.day.from_now.all_day).order('events.event_start_date, events.event_start_time ASC') }
-  scope :this_week, lambda do
+  
+  def this_week
     two_days = Time.now.beginning_of_day + 2.days
     one_week = Time.now.end_of_day + 7.days
     where('events.event_start_date >= ? AND events.event_start_date <= ?', two_days, one_week).order('events.event_start_date, events.event_start_time ASC')
