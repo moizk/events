db20a5aa07bafb1b087219ef236218474699aefd
Alexander E. Dimitriyadi <alex@heuretix.com>
2013-03-29 21:02:08 -0300

    Fixing weird rails bug with range of time values.

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,5 +4,9 @@ class Event < ActiveRecord::Base
 
   scope :today, lambda { where(event_start_date: Time.zone.now.all_day).order('events.event_start_time ASC') }
   scope :tomorrow, lambda { where(event_start_date: 1.day.from_now.all_day).order('events.event_start_date, events.event_start_time ASC') }
-  scope :this_week, lambda { where(event_start_date: 3.day.from_now.all_week).order('events.event_start_date, events.event_start_time ASC') }
+  scope :this_week, lambda do
+    two_days = Time.now.beginning_of_day + 2.days
+    one_week = Time.now.end_of_day + 7.days
+    where('events.event_start_date >= ? AND events.event_start_date <= ?', two_days, one_week).order('events.event_start_date, events.event_start_time ASC')
+  end
 end
